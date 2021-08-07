08c49d37dfdb81f16a1b82172a5c3ebb570bcdd6
masoumehaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-08-07 13:06:13 +0430

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,15 @@
+
+
+s = (int(input("enter your number for lenghth snake :")))
+
+
+
+for i in range( s):
+
+    if i % 2 == 0:
+        print ("*" , end ='')
+    else  :
+         print ("#" ,end='')   
+ 
+
+#print ("#" , end='')
